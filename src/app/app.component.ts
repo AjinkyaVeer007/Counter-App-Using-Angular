0b14dfcd64444d9cfea60a076b0deb68a6ec7271
AjinkyaVeer <veerajinkya1@gmail.com>
2023-03-01 20:44:48 +0530

    first angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title : string = 'Counter-App';
+  name: string = "Ajinkya";
+
+  count: number = 0
+
+  increaseCount=()=> {
+    this.count = this.count + 1
+  }
+
+  decreaseCount = () => {
+    if(this.count>0){ this.count = this.count - 1}
+   
+  }
+
+  resetCount = () => {
+    this.count = 0
+  }
 }
